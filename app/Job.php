0d1eef6650b17a11b3dcf53a98dcf049a9c19e08
JC Ricaro <jcricaro@gmail.com>
2016-08-29 09:14:43 +0000

    Describe job

--- app/Job.php
+++ app/Job.php
@@ -6,5 +6,121 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+    /**
+     * Tables name
+     *
+     * @var string
+     */
+    protected $table = 'jobs';
+
+    /**
+     * Fillable fields
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'phone',
+        'service_type',
+        'kilogram',
+        'washer_mode',
+        'dryer_mode',
+        'detergent',
+        'bleach',
+        'fabric_conditioner',
+        'is_press',
+        'is_fold'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'is_press' => 'boolean',
+        'is_fold' => 'boolean'
+    ];
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function getServiceTypeAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function getKilogramAttribute($value)
+    {
+        return $value . ' kg';
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function getWasherModeAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function getDryerModeAttribute($value)
+    {
+        return $value . ' mins';
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function getDetergentAttribute($value)
+    {
+        return ucfirst( str_replace('_', ' ', $value) );
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function getBleachAttribute($value)
+    {
+        return ucfirst( str_replace('_', ' ', $value) );
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function getFabricConditionerAttribute($value)
+    {
+        return ucfirst( str_replace('_', ' ', $value) );
+    }
+
+    
+    public function scopeApproved($query)
+    {
+        return $query->where('status', 'approved');
+    }
+
+    public function scopeDeclined($query)
+    {
+        return $query->where('status', 'declined');
+    }
+
+    public function scopeDone($query)
+    {
+        return $query->where('status', 'done');
+    }
+
+    public function scopePending($query)
+    {
+        return $query->where('status', 'pending');
+    }
 }
