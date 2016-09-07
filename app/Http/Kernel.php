fca2cde6606f31349430f4b1f2718484d0a17684
JC Ricaro <jcricaro@gmail.com>
2016-09-07 10:55:37 +0000

    CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -36,6 +35,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
