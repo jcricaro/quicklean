104d0fe5f34216ae98261c164a77125fb27cac80
JC Ricaro <jcricaro@gmail.com>
2016-09-17 00:14:32 +0000

    Walkin phone validation

--- app/Http/Requests/Job/AddJobWalkinRequest.php
+++ app/Http/Requests/Job/AddJobWalkinRequest.php
@@ -25,7 +25,7 @@ class AddJobWalkinRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'phone' => 'required'
+            'phone' => 'required|digits:11'
         ];
     }
 }
