a8a6cb6a5f05dace30d14ffd94fecdde1cbb197c
JC Ricaro <jcricaro@gmail.com>
2016-09-17 00:14:15 +0000

    Phone number validation

--- app/Http/Requests/Job/AddJobReservationRequest.php
+++ app/Http/Requests/Job/AddJobReservationRequest.php
@@ -25,7 +25,7 @@ class AddJobReservationRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'phone' => 'required',
+            'phone' => 'required|digits:11',
             'reserve_at' => 'required|date'
         ];
     }
